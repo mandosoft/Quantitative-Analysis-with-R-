21fcd553f9708fc5405f59de2c8f3127f82f4596
Thomas Townsley <thomasdtownsley@gmail.com>
2018-12-11 11:16:46 -0600

    basic updates

--- R Analysis.R
+++ R Analysis.R
@@ -1,3 +1,9 @@
+library(quantmod)
 require(quantmod)
 require(RODBC)
 require(dplyr)
+
+AAPL <- getSymbols("AAPL", auto.assign = FALSE)
+
+candleChart(AAPL, multi.col = TRUE, theme = 'white', subset = '2017::2018')
+
