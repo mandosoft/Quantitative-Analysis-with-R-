a2e58eb86e4b00c37dbfaa5bf1be96c464d0e0a7
Thomas Townsley <thomasdtownsley@gmail.com>
2018-12-11 11:40:04 -0600

    changes

--- R Analysis.R
+++ R Analysis.R
@@ -1,9 +1,25 @@
-library(quantmod)
+
 require(quantmod)
 require(RODBC)
 require(dplyr)
+require(urca)
+require(GGally)
+
+#Exploratory analysis between AAPL and VIX 
 
 AAPL <- getSymbols("AAPL", auto.assign = FALSE)
+VIX <- getSymbols("VIX", auto.assign = FALSE)
 
+
+prices1 <- AAPL$AAPL.Close
+
+#compute log prices to analyze stationarity 
+returns1 <- diff(log(prices1))
+scatter.smooth(returns1)
+
+#corr plot for visuals
+ggcorr(AAPL, VIX)
+
+View(VIX)
 candleChart(AAPL, multi.col = TRUE, theme = 'white', subset = '2017::2018')
 
